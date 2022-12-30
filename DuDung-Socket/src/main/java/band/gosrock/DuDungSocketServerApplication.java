78d62834c6b345b845ae2b29959bd96988c2fceb
이찬진 <water0641@naver.com>
2022-12-31 02:39:17 +0900

    init : [Socket] 소켓 관련 서버 세팅

---
+++ DuDung-Socket/src/main/java/band/gosrock/DuDungSocketServerApplication.java
@@ -0,0 +1,28 @@
+package band.gosrock;
+
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class DuDungSocketServerApplication implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuDungSocketServerApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        log.info("applicationReady status" + Arrays.toString(environment.getActiveProfiles()));
+    }
+}
