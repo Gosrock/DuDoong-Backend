cf9b514e8819a136dfa72e561908e16c4924ee59
이찬진 <water0641@naver.com>
2022-12-31 16:52:37 +0900

    feat(domain/user) : 프로필 변경 테스트

--- DuDung-Domain/src/main/java/band/gosrock/domain/user/domain/Profile.java
+++ DuDung-Domain/src/main/java/band/gosrock/domain/user/domain/Profile.java
@@ -3,9 +3,11 @@ package band.gosrock.domain.user.domain;
 import javax.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profile {
     private String name;
