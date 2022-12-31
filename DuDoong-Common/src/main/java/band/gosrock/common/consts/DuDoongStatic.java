2cda7d83c917653271f13a54eaebf6961bfa912d
이찬진 <water0641@naver.com>
2023-01-01 02:29:08 +0900

    feat(common) : JwtTokenProvider 추가

--- DuDoong-Common/src/main/java/band/gosrock/common/consts/DuDoongStatic.java
+++ DuDoong-Common/src/main/java/band/gosrock/common/consts/DuDoongStatic.java
@@ -4,4 +4,6 @@ public class DuDoongConsts {
     public static final String AUTH_HEADER = "Authorization";
     public static final String BEARER = "Bearer";
     public static final String WITHDRAW_PREFIX = "DELETED:";
+    public static final String TOKEN_ROLE = "role";
+    public static final String TOKEN_TYPE = "type";
 }
