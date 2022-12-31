e1bac2f8b99b997e738ebf1c3b4d38947e1bbfb4
이찬진 <water0641@naver.com>
2022-12-31 16:53:19 +0900

    feat(common/consts) : DuDoongConsts 추가

--- DuDung-Common/src/main/java/band/gosrock/common/consts/DuDoongConsts.java
+++ DuDung-Common/src/main/java/band/gosrock/common/consts/DuDoongConsts.java
@@ -1,5 +1,8 @@
-package band.gosrock.common.constant;
+package band.gosrock.common.consts;
 
-public class DuDoongStatic {
+public class DuDoongConsts {
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String BEARER = "Bearer";
+    public static final String WITHDRAW_PREFIX = "DELETED:";
 
 }
