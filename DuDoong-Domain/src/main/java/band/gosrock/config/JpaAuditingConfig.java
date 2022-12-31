4868886f19a84d8696da7d5c8d1ea33d1c3764c8
이찬진 <water0641@naver.com>
2022-12-31 18:55:02 +0900

    feat(domain/config) : jpa Auditing 추가

---
+++ DuDoong-Domain/src/main/java/band/gosrock/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package band.gosrock.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {}
