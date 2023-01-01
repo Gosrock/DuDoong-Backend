9a5db921bd48bc15243b96e1036b283765a604bf
이찬진 <water0641@naver.com>
2023-01-01 20:22:30 +0900

    feat(common) : kakao oauth properties

--- DuDoong-Common/src/main/java/band/gosrock/common/config/ConfigurationPropertiesConfig.java
+++ DuDoong-Common/src/main/java/band/gosrock/common/config/ConfigurationPropertiesConfig.java
@@ -2,9 +2,10 @@ package band.gosrock.common.config;
 
 
 import band.gosrock.common.properties.JwtProperties;
+import band.gosrock.common.properties.OauthProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({JwtProperties.class})
+@EnableConfigurationProperties({JwtProperties.class, OauthProperties.class})
 @Configuration
 public class ConfigurationPropertiesConfig {}
--- DuDoong-Common/src/main/java/band/gosrock/common/properties/OauthProperties.java
+++ DuDoong-Common/src/main/java/band/gosrock/common/properties/OauthProperties.java
@@ -1,2 +1,47 @@
-package band.gosrock.common.properties;public class OauthProperties {
+package band.gosrock.common.properties;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties("oauth")
+public class OauthProperties {
+
+    private OAuthSecret kakao;
+
+    @Getter
+    @Setter
+    public static class OAuthSecret {
+        private String baseUrl;
+        private String clientId;
+        private String clientSecret;
+        private String redirectUrl;
+        private String appId;
+    }
+
+    public String getKakaoBaseUrl() {
+        return kakao.getBaseUrl();
+    }
+
+    public String getKakaoClientId() {
+        return kakao.getClientId();
+    }
+
+    public String getKakaoRedirectUrl() {
+        return kakao.getRedirectUrl();
+    }
+
+    public String getKakaoClientSecret() {
+        return kakao.getClientSecret();
+    }
+
+    public String getKakaoAppId() {
+        return kakao.getAppId();
+    }
 }
