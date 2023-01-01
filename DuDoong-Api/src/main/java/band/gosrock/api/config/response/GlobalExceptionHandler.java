32104b75c93075fe745032c5f37a1fafe76e9383
이찬진 <water0641@naver.com>
2023-01-02 01:17:07 +0900

    feat(api) : ErrorResponse

--- DuDoong-Api/src/main/java/band/gosrock/api/config/response/GlobalExceptionHandler.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/config/response/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package band.gosrock.api.config.response;
 
+
+import band.gosrock.common.dto.ErrorResponse;
 import band.gosrock.common.exception.DuDoongCodeException;
 import band.gosrock.common.exception.ErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,81 +34,81 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(
-        Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+            Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ServletWebRequest servletWebRequest = (ServletWebRequest) request;
 
         String url =
-            UriComponentsBuilder.fromHttpRequest(
-                    new ServletServerHttpRequest(servletWebRequest.getRequest()))
-                .build()
-                .toUriString();
+                UriComponentsBuilder.fromHttpRequest(
+                                new ServletServerHttpRequest(servletWebRequest.getRequest()))
+                        .build()
+                        .toUriString();
 
         ErrorResponse errorResponse =
-            new ErrorResponse(status.value(), status.name(), ex.getMessage(), url);
+                new ErrorResponse(status.value(), status.name(), ex.getMessage(), url);
         return super.handleExceptionInternal(ex, errorResponse, headers, status, request);
     }
 
     @SneakyThrows
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
-        MethodArgumentNotValidException ex,
-        HttpHeaders headers,
-        HttpStatus status,
-        WebRequest request) {
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
 
         List<FieldError> errors = ex.getBindingResult().getFieldErrors();
         ServletWebRequest servletWebRequest = (ServletWebRequest) request;
         String url =
-            UriComponentsBuilder.fromHttpRequest(
-                    new ServletServerHttpRequest(servletWebRequest.getRequest()))
-                .build()
-                .toUriString();
+                UriComponentsBuilder.fromHttpRequest(
+                                new ServletServerHttpRequest(servletWebRequest.getRequest()))
+                        .build()
+                        .toUriString();
         Map<String, Object> fieldAndErrorMessages =
-            errors.stream()
-                .collect(
-                    Collectors.toMap(
-                        FieldError::getField, FieldError::getDefaultMessage));
+                errors.stream()
+                        .collect(
+                                Collectors.toMap(
+                                        FieldError::getField, FieldError::getDefaultMessage));
 
         String errorsToJsonString = new ObjectMapper().writeValueAsString(fieldAndErrorMessages);
         ErrorResponse errorResponse =
-            new ErrorResponse(status.value(), status.name(), errorsToJsonString, url);
+                new ErrorResponse(status.value(), status.name(), errorsToJsonString, url);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(DuDoongCodeException.class)
     public ResponseEntity<ErrorResponse> KnockExceptionHandler(
-        DuDoongCodeException e, HttpServletRequest request) {
+            DuDoongCodeException e, HttpServletRequest request) {
         ErrorCode code = e.getErrorCode();
         ErrorResponse errorResponse =
-            new ErrorResponse(
-                code.getStatus(),
-                code.getCode(),
-                code.getReason(),
-                request.getRequestURL().toString());
+                new ErrorResponse(
+                        code.getStatus(),
+                        code.getCode(),
+                        code.getReason(),
+                        request.getRequestURL().toString());
         return ResponseEntity.status(HttpStatus.valueOf(code.getStatus())).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest request)
-        throws IOException {
+            throws IOException {
         final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
         String url =
-            UriComponentsBuilder.fromHttpRequest(new ServletServerHttpRequest(request))
-                .build()
-                .toUriString();
+                UriComponentsBuilder.fromHttpRequest(new ServletServerHttpRequest(request))
+                        .build()
+                        .toUriString();
 
         log.error("INTERNAL_SERVER_ERROR", e);
         ErrorCode internalServerError = ErrorCode.INTERNAL_SERVER_ERROR;
         ErrorResponse errorResponse =
-            new ErrorResponse(
-                internalServerError.getStatus(),
-                internalServerError.getCode(),
-                internalServerError.getReason(),
-                url);
-        //TODO : 슬랙 오류 발송
-//        slackProvider.sendError(url, cachingRequest, e);
+                new ErrorResponse(
+                        internalServerError.getStatus(),
+                        internalServerError.getCode(),
+                        internalServerError.getReason(),
+                        url);
+        // TODO : 슬랙 오류 발송
+        //        slackProvider.sendError(url, cachingRequest, e);
         return ResponseEntity.status(HttpStatus.valueOf(internalServerError.getStatus()))
-            .body(errorResponse);
+                .body(errorResponse);
     }
 }
