f8fc4f6b6c92e3af0f08a2c92c9c15f8875c5f42
이찬진 <water0641@naver.com>
2023-01-02 19:38:17 +0900

    feat(common) : Block api aop 추가

---
+++ DuDoong-Common/src/main/java/band/gosrock/common/aop/ApiBlockingAspect.java
@@ -0,0 +1,28 @@
+package band.gosrock.common.aop;
+
+import band.gosrock.common.exception.DuDoongDynamicException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ApiBlockingAspect {
+
+    @Value("${spring.config.activate.on-profile}")
+    private String profile;
+
+    @Around("@annotation(band.gosrock.common.annotation.DevelopOnlyApi)")
+    public Object checkApiAcceptingCondition(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[ABA::checkApiAcceptingCondition] environment={}", profile);
+        if(profile.equals("prod")) {
+            throw new DuDoongDynamicException(405,"Blocked Api","not working api in production");
+        }
+        return joinPoint.proceed();
+
+    }
+}
