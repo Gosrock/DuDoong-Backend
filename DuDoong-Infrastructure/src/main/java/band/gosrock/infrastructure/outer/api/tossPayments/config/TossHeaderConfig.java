2fb5a93fce8cabb5fc587a04399ae744ae41745f
이찬진 <water0641@naver.com>
2023-01-03 22:10:30 +0900

    fix(infa) : toss headerConfig 에서 component 제거

--- DuDoong-Infrastructure/src/main/java/band/gosrock/infrastructure/outer/api/tossPayments/config/TossHeaderConfig.java
+++ DuDoong-Infrastructure/src/main/java/band/gosrock/infrastructure/outer/api/tossPayments/config/TossHeaderConfig.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component
 @RequiredArgsConstructor
 public class TossHeaderConfig {
 
