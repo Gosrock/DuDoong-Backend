df50893ab2b1794de5afa32f8367f13ba0b6c6f5
Chan Jin <water0641@naver.com>
2023-01-04 23:15:02 +0900

    feat(infra) : s3 presigned url 로 연동(#50)
    * feat(infra) : aws config
    
    * feat(infra) : s3 presignedService
    
    * feat(infra) : s3 연동 테스트 완료
    
    * style: spotless
    
    * fix(infra) : application-infrastructure.yml 업데이트
    
    * style : chang -> change 로 수정

---
+++ DuDoong-Infrastructure/src/test/java/band/gosrock/infrastructure/config/s3/S3UploadPresignedUrlServiceTest.java
@@ -0,0 +1,16 @@
+package band.gosrock.infrastructure.config.s3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// @SpringBootTest(classes = {S3Config.class, S3UploadPresignedUrlService.class})
+// @ActiveProfiles("infrastructure")
+// class S3UploadPresignedUrlServiceTest {
+//
+//    @Autowired
+//    S3UploadPresignedUrlService s3UploadPresignedUrlService;
+//
+//    @Test
+//    public void S3_url_발급테스트() {
+//        String url = s3UploadPresignedUrlService.execute(1L,"jpg");
+//    }
+// }
