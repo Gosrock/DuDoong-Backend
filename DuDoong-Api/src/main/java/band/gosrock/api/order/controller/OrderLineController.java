5ff7729aebe21e57432ea767539eb7ff820f2516
이찬진 <water0641@naver.com>
2023-01-06 18:23:01 +0900

    feat(api) : orderLine recent 관련 url 추가

--- DuDoong-Api/src/main/java/band/gosrock/api/order/controller/OrderLineController.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/order/controller/OrderLineController.java
@@ -2,6 +2,7 @@ package band.gosrock.api.order.controller;
 
 
 import band.gosrock.api.order.service.CreateOrderLineUseCase;
+import band.gosrock.api.order.service.ReadOrderLineUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,11 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderLineController {
 
     private CreateOrderLineUseCase createOrderLineUseCase;
-
+    private ReadOrderLineUseCase readOrderLineUseCase;
     @Operation(summary = "티켓상품의 갯수와 사용자가 입력한 옵션을 가진 orderLine 을 만듭니다.")
-    @PostMapping("ticketItems/{ticketItem_id}")
-    public void createOrder(@PathVariable("ticketItem_id") String ticketItemId) {
+    @PostMapping("/ticketItems/{ticketItem_id}")
+    public void createOrderLine(@PathVariable("ticketItem_id") String ticketItemId) {
         createOrderLineUseCase.execute();
+//        return oauthUserInfoUseCase.execute(accessToken);
+    }
+
+    @Operation(summary = "사용자가 최근에 만들었던 을 불러옵니다. 없으면 data null (구현 안해도 됨)")
+    @PostMapping("/ticketItems/recent")
+    public void createOrder() {
+        readOrderLineUseCase.getRecentOrderLine();
 //        return oauthUserInfoUseCase.execute(accessToken);
     }
 }
---
+++ DuDoong-Api/src/main/java/band/gosrock/api/order/service/ReadOrderLineUseCase.java
@@ -0,0 +1,11 @@
+package band.gosrock.api.order.service;
+
+public class ReadOrderLineUseCase {
+
+    public void execute() {
+
+    }
+
+    public void getRecentOrderLine() {
+    }
+}
