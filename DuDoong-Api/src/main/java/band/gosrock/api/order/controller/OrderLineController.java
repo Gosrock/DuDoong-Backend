41b6b0327d5f478ebda22211c69b310d2441a016
이찬진 <water0641@naver.com>
2023-01-06 18:17:49 +0900

    feat(api) : orderLine 관련 컨트롤러 url 설정

--- DuDoong-Api/src/main/java/band/gosrock/api/order/controller/OrderLineController.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/order/controller/OrderLineController.java
@@ -1,8 +1,13 @@
 package band.gosrock.api.order.controller;
 
 
+import band.gosrock.api.order.service.CreateOrderLineUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "장바구니 관련 컨트롤러")
 @RestController
 @RequestMapping("/v1/orderLines")
+@RequiredArgsConstructor
 public class OrderLineController {
 
+    private CreateOrderLineUseCase createOrderLineUseCase;
+
+    @Operation(summary = "티켓상품의 갯수와 사용자가 입력한 옵션을 가진 orderLine 을 만듭니다.")
+    @PostMapping("ticketItems/{ticketItem_id}")
+    public void createOrder(@PathVariable("ticketItem_id") String ticketItemId) {
+        createOrderLineUseCase.execute();
+//        return oauthUserInfoUseCase.execute(accessToken);
+    }
 }
--- DuDoong-Api/src/main/java/band/gosrock/api/order/service/CreateOrderLineUseCase.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/order/service/CreateOrderLineUseCase.java
@@ -2,4 +2,7 @@ package band.gosrock.api.order.service;
 
 public class CreateOrderLineUseCase {
 
+    public void execute() {
+
+    }
 }
