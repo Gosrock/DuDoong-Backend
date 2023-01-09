7a51048644f90a43de9986d56be5070bb1e261d1
이찬진 <water0641@naver.com>
2023-01-09 16:50:57 +0900

    feat(domain/option) : option 관련 레포지토리 추가

---
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/ticket_item/repository/OptionRepository.java
@@ -0,0 +1,7 @@
+package band.gosrock.domain.domains.ticket_item.repository;
+
+
+import band.gosrock.domain.domains.ticket_item.domain.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {}
