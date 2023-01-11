78b8c7ea1a47d062b1110ab654305f5557453d75
김민준 <sanbonai06@naver.com>
2023-01-12 01:52:48 +0900

    refactor(api) : 발급 티켓 리스트 가져오기 API 어드민 컨트롤러로 분리 및 경로 변경

--- DuDoong-Api/src/main/java/band/gosrock/api/issuedTicket/controller/IssuedTicketController.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/issuedTicket/controller/IssuedTicketController.java
@@ -2,10 +2,8 @@ package band.gosrock.api.issuedTicket.controller;
 
 
 import band.gosrock.api.issuedTicket.dto.response.RetrieveIssuedTicketDetailResponse;
-import band.gosrock.api.issuedTicket.dto.response.RetrieveIssuedTicketListResponse;
 import band.gosrock.api.issuedTicket.service.CreateIssuedTicketUseCase;
 import band.gosrock.api.issuedTicket.service.ReadIssuedTicketUseCase;
-import band.gosrock.api.issuedTicket.service.ReadIssuedTicketsUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SecurityRequirement(name = "access-token")
@@ -25,20 +22,10 @@ public class IssuedTicketController {
 
     private final CreateIssuedTicketUseCase createIssuedTicketUseCase;
     private final ReadIssuedTicketUseCase readIssuedTicketUseCase;
-    private final ReadIssuedTicketsUseCase readIssuedTicketsUseCase;
 
     @Operation(summary = "발급 티켓 가져오기 API 입니다.")
     @GetMapping(value = "/{issuedTicketId}", produces = "application/json; charset=utf-8")
     public RetrieveIssuedTicketDetailResponse getIssuedTicket(@PathVariable Long issuedTicketId) {
         return readIssuedTicketUseCase.execute(issuedTicketId);
-    }
-
-    @Operation(summary = "[어드민 기능] 발급 티켓 리스트 가져오기 API 입니다.")
-    @GetMapping
-    public RetrieveIssuedTicketListResponse getIssuedTickets(
-            @RequestParam Long page,
-            @RequestParam Long eventId,
-            @RequestParam(required = false) String userName) {
-        return readIssuedTicketsUseCase.execute(page, eventId, userName);
     }
 }
