a2ec6e4cc8c076106f968ee25172085ebca95441
이찬진 <water0641@naver.com>
2023-01-11 16:47:55 +0900

    feat(infra) : 페이먼트 상태 추가

--- DuDoong-Infrastructure/src/main/java/band/gosrock/infrastructure/outer/api/tossPayments/dto/response/PaymentsResponse.java
+++ DuDoong-Infrastructure/src/main/java/band/gosrock/infrastructure/outer/api/tossPayments/dto/response/PaymentsResponse.java
@@ -31,23 +31,8 @@ public class PaymentsResponse {
     private Long totalAmount;
     // 취소할 수 있는 금액(잔고)
     private Long balanceAmount;
-    // 결제 처리 상태
-    // - READY: 결제를 생성하면 가지게 되는 초기 상태 입니다. 인증 전까지는 READY 상태를 유지합니다.
-    //
-    // - IN_PROGRESS: 결제 수단 정보와 해당 결제 수단의 소유자가 맞는지 인증을 마친 상태입니다. 결제 승인 API를 호출하면 결제가 완료됩니다.
-    //
-    // - WAITING_FOR_DEPOSIT: 가상계좌 결제 흐름에만 있는 상태로, 결제 고객이 발급된 가상계좌에 입금하는 것을 기다리고 있는 상태입니다.
-    //
-    // - DONE: 인증된 결제 수단 정보, 고객 정보로 요청한 결제가 승인된 상태입니다.
-    //
-    // - CANCELED: 승인된 결제가 취소된 상태입니다.
-    //
-    // - PARTIAL_CANCELED: 승인된 결제가 부분 취소된 상태입니다.
-    //
-    // - ABORTED: 결제 승인이 실패했거나, 결제 고객이 창을 닫아서 결제를 취소한 상태입니다.
-    //
-    // - EXPIRED: 결제 유효 시간 30분이 지나 거래가 취소된 상태입니다. IN_PROGRESS 상태에서 결제 승인 API를 호출하지 않으면 EXPIRED가 됩니다
-    private String status;
+
+    private PaymentStatus status;
 
     // 결제 일어난 시간정보
     private ZonedDateTime requestedAt;
