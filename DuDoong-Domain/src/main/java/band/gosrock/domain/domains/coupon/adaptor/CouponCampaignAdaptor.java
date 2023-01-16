e8847651ca83fc3181c47f9ab801cf3dd3c17c4b
bryn <cofls6581@naver.com>
2023-01-16 23:03:42 +0900

    refactor : CouponCampaignAdaptor 생성

---
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/coupon/adaptor/CouponCampaignAdaptor.java
@@ -0,0 +1,23 @@
+package band.gosrock.domain.domains.coupon.adaptor;
+
+
+import band.gosrock.common.annotation.Adaptor;
+import band.gosrock.domain.domains.coupon.domain.CouponCampaign;
+import band.gosrock.domain.domains.coupon.exception.AlreadyExistCouponCampaignException;
+import band.gosrock.domain.domains.coupon.repository.CouponCampaignRepository;
+import lombok.RequiredArgsConstructor;
+
+@Adaptor
+@RequiredArgsConstructor
+public class CouponCampaignAdaptor {
+    private final CouponCampaignRepository couponCampaignRepository;
+
+    public CouponCampaign save(CouponCampaign couponCampaign) {
+        return couponCampaignRepository.save(couponCampaign);
+    }
+
+    public void existsByCouponCode(String couponCode) {
+        if (couponCampaignRepository.existsByCouponCode(couponCode))
+            throw AlreadyExistCouponCampaignException.EXCEPTION;
+    }
+}
