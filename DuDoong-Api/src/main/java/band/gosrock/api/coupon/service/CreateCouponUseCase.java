8acb19650388d96aafbae6b565812d568f4b278b
bryn <cofls6581@naver.com>
2023-01-19 22:40:09 +0900

    feat : 유저유틸 적용

--- DuDoong-Api/src/main/java/band/gosrock/api/coupon/service/CreateCouponUseCase.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/coupon/service/CreateCouponUseCase.java
@@ -1,13 +1,13 @@
 package band.gosrock.api.coupon.service;
 
 
-import band.gosrock.api.config.security.SecurityUtils;
+import band.gosrock.api.common.UserUtils;
 import band.gosrock.api.coupon.dto.reqeust.CreateCouponCampaignRequest;
 import band.gosrock.api.coupon.dto.response.CreateCouponCampaignResponse;
 import band.gosrock.common.annotation.UseCase;
 import band.gosrock.domain.domains.coupon.domain.CouponCampaign;
 import band.gosrock.domain.domains.coupon.service.CreateCouponCampaignDomainService;
-import band.gosrock.domain.domains.user.service.UserDomainService;
+import band.gosrock.domain.domains.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,14 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CreateCouponUseCase {
 
-    private final UserDomainService userDomainService;
+    private final UserUtils userUtils;
     private final CreateCouponCampaignDomainService createCouponCampaignDomainService;
 
     @Transactional
     public CreateCouponCampaignResponse execute(
             CreateCouponCampaignRequest createCouponCampaignRequest) {
         // 존재하는 유저인지 검증
-        userDomainService.retrieveUser(SecurityUtils.getCurrentUserId());
+        User user = userUtils.getCurrentUser();
         // 슈퍼 호스트인지 검증
         // TODO : 차후 호스트쪽 코드 구조 나오는 거 보고 넣을 예정
         // 이미 생성된 쿠폰 코드인지 검증
