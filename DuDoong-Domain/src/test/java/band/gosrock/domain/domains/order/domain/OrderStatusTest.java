d8cbb650fb82ceacb75fcef81162f40557bd4c3c
이찬진 <water0641@naver.com>
2023-01-22 20:59:00 +0900

    test : OrderOptionAnswerTest

--- DuDoong-Domain/src/test/java/band/gosrock/domain/domains/order/domain/OrderStatusTest.java
+++ DuDoong-Domain/src/test/java/band/gosrock/domain/domains/order/domain/OrderStatusTest.java
@@ -13,79 +13,76 @@ import org.junit.jupiter.api.function.Executable;
 
 class OrderStatusTest {
 
-
     @Test
-    void 주문상태_취소가능_상태_확인(){
+    void 주문상태_취소가능_상태_확인() {
         OrderStatus.CONFIRM.validCanCancel();
         OrderStatus.APPROVED.validCanCancel();
     }
 
     @Test
-    void 주문상태_취소_불가능_상태_확인(){
-        //given
-        List<Executable> executables = Arrays.stream(OrderStatus.values())
-            .filter(orderStatus -> !canWithDrawStatus(orderStatus))
-            .<Executable>map(orderStatus -> orderStatus::validCanCancel
-            ).toList();
-        //then
+    void 주문상태_취소_불가능_상태_확인() {
+        // given
+        List<Executable> executables =
+                Arrays.stream(OrderStatus.values())
+                        .filter(orderStatus -> !canWithDrawStatus(orderStatus))
+                        .<Executable>map(orderStatus -> orderStatus::validCanCancel)
+                        .toList();
+        // then
         executables.forEach(
-            executable->
-                assertThrows(CanNotCancelOrderException.class, executable));
+                executable -> assertThrows(CanNotCancelOrderException.class, executable));
     }
 
     @Test
-    void 주문상태_환불가능_상태_확인(){
+    void 주문상태_환불가능_상태_확인() {
         OrderStatus.CONFIRM.validCanRefund();
         OrderStatus.APPROVED.validCanRefund();
     }
 
     @Test
-    void 주문상태_환불_불가능_상태_확인(){
-        //given
-        List<Executable> executables = Arrays.stream(OrderStatus.values())
-            .filter(orderStatus -> !canWithDrawStatus(orderStatus))
-            .<Executable>map(orderStatus -> orderStatus::validCanRefund
-            ).toList();
-        //then
+    void 주문상태_환불_불가능_상태_확인() {
+        // given
+        List<Executable> executables =
+                Arrays.stream(OrderStatus.values())
+                        .filter(orderStatus -> !canWithDrawStatus(orderStatus))
+                        .<Executable>map(orderStatus -> orderStatus::validCanRefund)
+                        .toList();
+        // then
         executables.forEach(
-            executable->
-                assertThrows(CanNotRefundOrderException.class, executable));
+                executable -> assertThrows(CanNotRefundOrderException.class, executable));
     }
 
     @Test
-    void 주문상태_결제방식_승인_상태_확인(){
+    void 주문상태_결제방식_승인_상태_확인() {
         OrderStatus.PENDING_PAYMENT.validCanPaymentConfirm();
     }
 
     @Test
-    void 주문상태_결제방식_승인_불가능_상태_확인(){
-        //given
-        List<Executable> executables = Arrays.stream(OrderStatus.values())
-            .filter(orderStatus -> !orderStatus.equals(OrderStatus.PENDING_PAYMENT))
-            .<Executable>map(orderStatus -> orderStatus::validCanPaymentConfirm
-            ).toList();
-        //then
-        executables.forEach(
-            executable->
-                assertThrows(NotPendingOrderException.class, executable));
+    void 주문상태_결제방식_승인_불가능_상태_확인() {
+        // given
+        List<Executable> executables =
+                Arrays.stream(OrderStatus.values())
+                        .filter(orderStatus -> !orderStatus.equals(OrderStatus.PENDING_PAYMENT))
+                        .<Executable>map(orderStatus -> orderStatus::validCanPaymentConfirm)
+                        .toList();
+        // then
+        executables.forEach(executable -> assertThrows(NotPendingOrderException.class, executable));
     }
 
     @Test
-    void 주문상태_승인방식_승인_상태_확인(){
+    void 주문상태_승인방식_승인_상태_확인() {
         OrderStatus.PENDING_APPROVE.validCanApprove();
     }
 
     @Test
-    void 주문상태_승인방식_승인_불가능_상태_확인(){
-        //given
-        List<Executable> executables = Arrays.stream(OrderStatus.values())
-            .filter(orderStatus -> !orderStatus.equals(OrderStatus.PENDING_APPROVE))
-            .<Executable>map(orderStatus -> orderStatus::validCanApprove
-            ).toList();
-        //then
-        executables.forEach(
-            executable->
-                assertThrows(NotPendingOrderException.class, executable));
+    void 주문상태_승인방식_승인_불가능_상태_확인() {
+        // given
+        List<Executable> executables =
+                Arrays.stream(OrderStatus.values())
+                        .filter(orderStatus -> !orderStatus.equals(OrderStatus.PENDING_APPROVE))
+                        .<Executable>map(orderStatus -> orderStatus::validCanApprove)
+                        .toList();
+        // then
+        executables.forEach(executable -> assertThrows(NotPendingOrderException.class, executable));
     }
 
     private static boolean canWithDrawStatus(OrderStatus orderStatus) {
--- DuDoong-Domain/src/test/java/band/gosrock/domain/domains/order/service/OrderApproveServiceTest.java
+++ DuDoong-Domain/src/test/java/band/gosrock/domain/domains/order/service/OrderApproveServiceTest.java
@@ -12,33 +12,28 @@ import band.gosrock.domain.domains.order.domain.Order;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OrderApproveServiceTest {
 
+    @Mock private OrderAdaptor orderAdaptor;
 
-    @Mock
-    private OrderAdaptor orderAdaptor;
-
-    @Mock
-    private Order order;
+    @Mock private Order order;
 
     @Test
     @DisplayName("주문승인_승인로직_한번만_호출해야한다.")
-    void 주문승인_승인로직_한번만_호출(){
-        //given
+    void 주문승인_승인로직_한번만_호출() {
+        // given
         willDoNothing().given(order).approve();
         given(orderAdaptor.findByOrderUuid(any())).willReturn(order);
         OrderApproveService orderApproveService = new OrderApproveService(orderAdaptor);
 
-
-        //when
+        // when
         orderApproveService.execute("uuid");
 
-        //then
+        // then
         then(order).should(times(1)).approve();
     }
 }
