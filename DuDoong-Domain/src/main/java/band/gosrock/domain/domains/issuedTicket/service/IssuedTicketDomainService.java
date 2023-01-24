631fea5bb9c96f8f10950bab61427e3d3b5a5c19
이찬진 <water0641@naver.com>
2023-01-24 20:24:50 +0900

    style : spotless

--- DuDoong-Domain/src/main/java/band/gosrock/domain/domains/issuedTicket/service/IssuedTicketDomainService.java
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/issuedTicket/service/IssuedTicketDomainService.java
@@ -4,17 +4,13 @@ package band.gosrock.domain.domains.issuedTicket.service;
 import band.gosrock.common.annotation.DomainService;
 import band.gosrock.domain.common.aop.redissonLock.RedissonLock;
 import band.gosrock.domain.common.vo.IssuedTicketInfoVo;
-import band.gosrock.domain.domains.event.adaptor.EventAdaptor;
 import band.gosrock.domain.domains.event.exception.HostNotAuthEventException;
 import band.gosrock.domain.domains.issuedTicket.adaptor.IssuedTicketAdaptor;
-import band.gosrock.domain.domains.issuedTicket.adaptor.IssuedTicketOptionAnswerAdaptor;
 import band.gosrock.domain.domains.issuedTicket.domain.IssuedTicket;
 import band.gosrock.domain.domains.issuedTicket.dto.request.CreateIssuedTicketDTO;
 import band.gosrock.domain.domains.issuedTicket.dto.response.CreateIssuedTicketResponse;
-import band.gosrock.domain.domains.issuedTicket.repository.IssuedTicketRepository;
 import band.gosrock.domain.domains.order.adaptor.OrderAdaptor;
 import band.gosrock.domain.domains.order.domain.Order;
-import band.gosrock.domain.domains.ticket_item.domain.TicketItem;
 import band.gosrock.domain.domains.user.adaptor.UserAdaptor;
 import band.gosrock.domain.domains.user.domain.User;
 import java.util.List;
@@ -26,29 +22,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class IssuedTicketDomainService {
-
-    private final IssuedTicketRepository issuedTicketRepository;
     private final IssuedTicketAdaptor issuedTicketAdaptor;
-    private final IssuedTicketOptionAnswerAdaptor issuedTicketOptionAnswerAdaptor;
-    private final EventAdaptor eventAdaptor;
     private final UserAdaptor userAdaptor;
     private final OrderAdaptor orderAdaptor;
 
     @RedissonLock(LockName = "티켓재고관리", identifier = "itemId")
     @Transactional
-    public void createIssuedTicket(
-            Long itemId, List<CreateIssuedTicketDTO> createIssuedTicketDTOs) {
-        createIssuedTicketDTOs.forEach(
-                dto -> {
-                    CreateIssuedTicketResponse responseDTO =
-                            IssuedTicket.orderLineItemToIssuedTickets(dto);
-                    issuedTicketAdaptor.saveAll(responseDTO.getIssuedTickets());
-                });
-    }
-
-    @RedissonLock(LockName = "티켓재고관리", paramClassType = TicketItem.class, identifier = "id")
-    @Transactional
-    public void withDrawIssuedTicket(TicketItem ticketItem, List<IssuedTicket> issuedTickets) {
+    public void withDrawIssuedTicket(Long itemId, List<IssuedTicket> issuedTickets) {
         issuedTickets.forEach(
                 issuedTicket -> {
                     issuedTicket.getTicketItem().increaseQuantity(1L);
--- DuDoong-Domain/src/main/java/band/gosrock/domain/domains/issuedTicket/service/handlers/OrderEventHandler.java
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/issuedTicket/service/handlers/OrderEventHandler.java
@@ -4,7 +4,6 @@ package band.gosrock.domain.domains.issuedTicket.service.handlers;
 import band.gosrock.domain.common.events.order.DoneOrderEvent;
 import band.gosrock.domain.domains.issuedTicket.service.IssuedTicketDomainService;
 import band.gosrock.domain.domains.order.adaptor.OrderAdaptor;
-import band.gosrock.domain.domains.order.domain.Order;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -26,7 +25,9 @@ public class OrderEventHandler {
     public void handleDoneOrderEvent(DoneOrderEvent doneOrderEvent) {
         log.info(doneOrderEvent.getOrderUuid() + "주문 상태 완료, 티켓 생성작업 진행");
         issuedTicketDomainService.createIssuedTicket(
-            doneOrderEvent.getItemId(), doneOrderEvent.getOrderUuid(), doneOrderEvent.getUserId());
+                doneOrderEvent.getItemId(),
+                doneOrderEvent.getOrderUuid(),
+                doneOrderEvent.getUserId());
         log.info(doneOrderEvent.getOrderUuid() + "주문 상태 완료, 티켓 생성작업 완료");
     }
 }
