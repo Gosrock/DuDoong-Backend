5570bef0a275a47953c71abc1da868e07d2eda4c
이찬진 <water0641@naver.com>
2023-01-26 20:05:49 +0900

    test : 주문취소 철회관련 테스팅 코드 추가

---
+++ DuDoong-Domain/src/test/java/band/gosrock/domain/domains/order/service/handler/WithDrawOrderHandlerTest.java
@@ -0,0 +1,41 @@
+package band.gosrock.domain.domains.order.service.handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+import band.gosrock.domain.common.events.order.WithDrawOrderEvent;
+import band.gosrock.domain.domains.order.adaptor.OrderAdaptor;
+import band.gosrock.domain.domains.order.domain.Order;
+import band.gosrock.domain.domains.order.service.WithdrawPaymentService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class WithDrawOrderHandlerTest {
+
+    @Mock OrderAdaptor orderAdaptor;
+
+    @Mock WithdrawPaymentService withdrawPaymentService;
+
+    @Mock WithDrawOrderEvent withDrawOrderEvent;
+
+    @Mock Order order;
+
+    @Test
+    public void 결제된_주문이_아니면_토스로_철회요청을_보내지_않는다() {
+        // given
+        WithDrawOrderHandler withDrawOrderHandler =
+                new WithDrawOrderHandler(withdrawPaymentService, orderAdaptor);
+        given(order.isPaid()).willReturn(Boolean.FALSE);
+        given(orderAdaptor.findByOrderUuid(any())).willReturn(order);
+        // when
+        withDrawOrderHandler.handleWithDrawOrderEvent(withDrawOrderEvent);
+        // then
+        then(withdrawPaymentService).should(times(0)).execute(any(), any(), any());
+    }
+}
