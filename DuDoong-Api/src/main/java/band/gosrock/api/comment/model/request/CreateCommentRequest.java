ff40394fad88d69276400c4f613eef3ed620c991
김민준 <sanbonai06@naver.com>
2023-01-26 12:55:54 +0900

    feat: 길이 밸리데이션 추가

--- DuDoong-Api/src/main/java/band/gosrock/api/comment/model/request/CreateCommentRequest.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/comment/model/request/CreateCommentRequest.java
@@ -2,14 +2,17 @@ package band.gosrock.api.comment.model.request;
 
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreateCommentRequest {
 
     @NotNull(message = "작성자 닉네임을 입력해주세요.")
+    @Size(min = 1, max = 15)
     private String nickName;
 
     @NotNull(message = "댓글 내용을 입력해주세요.")
+    @Size(min = 1, max = 200)
     private String content;
 }
