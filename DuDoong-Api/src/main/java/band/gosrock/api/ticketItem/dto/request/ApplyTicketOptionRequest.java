65d9953dae2944c97ba4cce43eced182a8738519
kim-wonjin <sobaek2000@naver.com>
2023-01-29 01:26:31 +0900

    refactor(itemOptionGroup) : 요청DTO 내 id검증 어노테이션 사용

--- DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/dto/request/ApplyTicketOptionRequest.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/dto/request/ApplyTicketOptionRequest.java
@@ -3,6 +3,8 @@ package band.gosrock.api.ticketItem.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,11 +12,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApplyTicketOptionRequest {
 
-    @NotNull
     @Schema(nullable = false, example = "1")
+    @Positive(message = "올바른 티켓상품 고유 아이디를 입력해주세요")
     private Long ticketItemId;
 
-    @NotNull
     @Schema(nullable = false, example = "1")
+    @Positive(message = "올바른 옵션그룹 고유 아이디를 입력해주세요")
     private Long optionGroupId;
 }
