e722751f37d612656f8f772788e0d2ca1f46bac6
이찬진 <water0641@naver.com>
2023-01-29 00:15:07 +0900

    test : cartValidator 테스트 코드 마저 추가

--- DuDoong-Domain/src/test/java/band/gosrock/domain/domains/cart/domain/CartValidatorTest.java
+++ DuDoong-Domain/src/test/java/band/gosrock/domain/domains/cart/domain/CartValidatorTest.java
@@ -7,13 +7,17 @@ import static org.mockito.BDDMockito.willCallRealMethod;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.BDDMockito.willThrow;
 
+import band.gosrock.domain.domains.cart.exception.CartInvalidOptionAnswerException;
 import band.gosrock.domain.domains.event.domain.Event;
 import band.gosrock.domain.domains.event.exception.EventIsNotOpenStatusException;
 import band.gosrock.domain.domains.event.exception.EventTicketingTimeIsPassedException;
 import band.gosrock.domain.domains.ticket_item.adaptor.OptionAdaptor;
 import band.gosrock.domain.domains.ticket_item.adaptor.TicketItemAdaptor;
+import band.gosrock.domain.domains.ticket_item.domain.Option;
 import band.gosrock.domain.domains.ticket_item.domain.TicketItem;
+import band.gosrock.domain.domains.ticket_item.exception.NotCorrectOptionAnswerException;
 import band.gosrock.domain.domains.ticket_item.exception.TicketItemQuantityLackException;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,7 +32,10 @@ class CartValidatorTest {
     @Mock OptionAdaptor optionAdaptor;
 
     @Mock Cart cart;
-
+    @Mock CartLineItem cartLineItem;
+    @Mock CartOptionAnswer cartOptionAnswer;
+    @Mock Option optionOfGroup1;
+    @Mock Option optionOfGroup2;
     @Mock Event event;
 
     @Mock TicketItem item;
@@ -104,10 +111,120 @@ class CartValidatorTest {
     @Test
     public void 카트_설문지_전부응답검증_성공() {
         // given
-        willThrow(EventIsNotOpenStatusException.class).given(event).validStatusOpen();
+        given(cart.getCartLineItems()).willReturn(List.of(cartLineItem));
+        given(optionAdaptor.findAllByIds(any()))
+                .willReturn(List.of(optionOfGroup1, optionOfGroup2));
+        Long optionGroup1Id = 1L;
+        Long optionGroup2Id = 2L;
+
+        given(optionOfGroup1.getOptionGroupId()).willReturn(optionGroup1Id);
+        given(optionOfGroup2.getOptionGroupId()).willReturn(optionGroup2Id);
+        given(item.getOptionGroupIds()).willReturn(List.of(optionGroup1Id, optionGroup2Id));
+        //        given(cartLineItem.getAnswerOptionIds())
+        // when
+        cartValidator.validAnswerToAllQuestion(cart, item);
+        // then
+    }
+
+    @Test
+    public void 카트_설문지_전부응답검증_적게대답_실패() {
+        // given
+        given(cart.getCartLineItems()).willReturn(List.of(cartLineItem));
+        given(optionAdaptor.findAllByIds(any()))
+                .willReturn(List.of(optionOfGroup1, optionOfGroup2));
+        Long optionGroup1Id = 1L;
+        Long optionGroup2Id = 2L;
+
+        given(optionOfGroup1.getOptionGroupId()).willReturn(optionGroup1Id);
+        given(optionOfGroup2.getOptionGroupId()).willReturn(optionGroup2Id);
+        Long optionGroup3Id = 3L;
+        given(item.getOptionGroupIds())
+                .willReturn(List.of(optionGroup1Id, optionGroup2Id, optionGroup3Id));
         // when
         // then
         assertThrows(
-                EventIsNotOpenStatusException.class, () -> cartValidator.validEventIsOpen(event));
+                CartInvalidOptionAnswerException.class,
+                () -> cartValidator.validAnswerToAllQuestion(cart, item));
+    }
+
+    @Test
+    public void 카트_설문지_전부응답검증_많게대답_실패() {
+        // given
+        given(cart.getCartLineItems()).willReturn(List.of(cartLineItem));
+        given(optionAdaptor.findAllByIds(any()))
+                .willReturn(List.of(optionOfGroup1, optionOfGroup2));
+        Long optionGroup1Id = 1L;
+        Long optionGroup2Id = 2L;
+
+        given(optionOfGroup1.getOptionGroupId()).willReturn(optionGroup1Id);
+        given(optionOfGroup2.getOptionGroupId()).willReturn(optionGroup2Id);
+        Long optionGroup3Id = 3L;
+        given(item.getOptionGroupIds()).willReturn(List.of(optionGroup1Id));
+        // when
+        // then
+        assertThrows(
+                CartInvalidOptionAnswerException.class,
+                () -> cartValidator.validAnswerToAllQuestion(cart, item));
+    }
+
+    @Test
+    public void 카트_설문지_전부응답검증_이상하게대답_실패() {
+        // given
+        given(cart.getCartLineItems()).willReturn(List.of(cartLineItem));
+        given(optionAdaptor.findAllByIds(any()))
+                .willReturn(List.of(optionOfGroup1, optionOfGroup2));
+        Long optionGroup1Id = 1L;
+        Long optionGroup2Id = 2L;
+
+        given(optionOfGroup1.getOptionGroupId()).willReturn(optionGroup1Id);
+        given(optionOfGroup2.getOptionGroupId()).willReturn(optionGroup2Id);
+        Long optionGroup3Id = 3L;
+        given(item.getOptionGroupIds()).willReturn(List.of(optionGroup1Id, optionGroup3Id));
+        // when
+        // then
+        assertThrows(
+                CartInvalidOptionAnswerException.class,
+                () -> cartValidator.validAnswerToAllQuestion(cart, item));
+    }
+
+    @Test
+    public void 카트_설문지_올바른응답검증_이상하게대답_실패() {
+        // given
+        given(cart.getCartLineItems()).willReturn(List.of(cartLineItem));
+        given(cartLineItem.getCartOptionAnswers()).willReturn(List.of(cartOptionAnswer));
+        Long optionId = 1L;
+        given(cartOptionAnswer.getOptionId()).willReturn(optionId);
+        given(cartOptionAnswer.getAnswer()).willReturn("이상하게대답");
+
+        given(optionAdaptor.findAllByIds(any())).willReturn(List.of(optionOfGroup1));
+
+        given(optionOfGroup1.getId()).willReturn(optionId);
+        willThrow(NotCorrectOptionAnswerException.class)
+                .given(optionOfGroup1)
+                .validCorrectAnswer("이상하게대답");
+        // when
+        // then
+        assertThrows(
+                NotCorrectOptionAnswerException.class,
+                () -> cartValidator.validCorrectAnswer(cart));
+    }
+
+    @Test
+    public void 카트_설문지_올바른응답검증_올바르게대답_성공() {
+        // given
+        given(cart.getCartLineItems()).willReturn(List.of(cartLineItem));
+        given(cartLineItem.getCartOptionAnswers()).willReturn(List.of(cartOptionAnswer));
+        Long optionId = 1L;
+        given(cartOptionAnswer.getOptionId()).willReturn(optionId);
+        given(cartOptionAnswer.getAnswer()).willReturn("예");
+
+        given(optionAdaptor.findAllByIds(any())).willReturn(List.of(optionOfGroup1));
+
+        given(optionOfGroup1.getId()).willReturn(optionId);
+        willDoNothing().given(optionOfGroup1).validCorrectAnswer("예");
+
+        // when
+        cartValidator.validCorrectAnswer(cart);
+        // then
     }
 }
