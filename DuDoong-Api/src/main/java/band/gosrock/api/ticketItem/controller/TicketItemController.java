f8e09e9d2a8a5e6486e517e3cf2a874632bad0d3
kim-wonjin <sobaek2000@naver.com>
2023-01-30 04:04:37 +0900

    feature(ticketItemOption) : 티켓상품 옵션 조회

--- DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/controller/TicketItemController.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/controller/TicketItemController.java
@@ -5,8 +5,10 @@ import band.gosrock.api.ticketItem.dto.request.ApplyTicketOptionRequest;
 import band.gosrock.api.ticketItem.dto.request.CreateTicketItemRequest;
 import band.gosrock.api.ticketItem.dto.response.ApplyTicketOptionResponse;
 import band.gosrock.api.ticketItem.dto.response.CreateTicketItemResponse;
+import band.gosrock.api.ticketItem.dto.response.GetTicketItemOptionResponse;
 import band.gosrock.api.ticketItem.service.ApplyTicketOptionUseCase;
 import band.gosrock.api.ticketItem.service.CreateTicketItemUseCase;
+import band.gosrock.api.ticketItem.service.GetTicketOptionsUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,6 +25,7 @@ public class TicketItemController {
 
     public final CreateTicketItemUseCase createTicketItemUseCase;
     public final ApplyTicketOptionUseCase applyTicketOptionUseCase;
+    public final GetTicketOptionsUseCase getTicketOptionsUseCase;
 
     @Operation(
             summary = "특정 이벤트에 속하는 티켓 상품을 생성합니다.",
@@ -41,5 +44,12 @@ public class TicketItemController {
             @PathVariable Long eventId,
             @PathVariable Long ticketItemId) {
         return applyTicketOptionUseCase.execute(applyTicketOptionRequest, eventId, ticketItemId);
+    }
+
+    @Operation(summary = "해당 티켓상품의 옵션을 모두 조회합니다.")
+    @GetMapping("/{ticketItemId}/options")
+    public GetTicketItemOptionResponse getTicketItemOptions(
+            @PathVariable Long eventId, @PathVariable Long ticketItemId) {
+        return getTicketOptionsUseCase.execute(eventId, ticketItemId);
     }
 }
--- DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/mapper/TicketOptionMapper.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/mapper/TicketOptionMapper.java
@@ -2,15 +2,25 @@ package band.gosrock.api.ticketItem.mapper;
 
 
 import band.gosrock.api.ticketItem.dto.request.CreateTicketOptionRequest;
+import band.gosrock.api.ticketItem.dto.response.GetTicketItemOptionResponse;
+import band.gosrock.api.ticketItem.dto.response.TicketOptionResponse;
 import band.gosrock.common.annotation.Mapper;
 import band.gosrock.domain.domains.event.domain.Event;
+import band.gosrock.domain.domains.ticket_item.adaptor.OptionGroupAdaptor;
+import band.gosrock.domain.domains.ticket_item.adaptor.TicketItemAdaptor;
 import band.gosrock.domain.domains.ticket_item.domain.OptionGroup;
+import band.gosrock.domain.domains.ticket_item.domain.TicketItem;
 import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Mapper
 @RequiredArgsConstructor
 public class TicketOptionMapper {
+
+    private final TicketItemAdaptor ticketItemAdaptor;
+    private final OptionGroupAdaptor optionGroupAdaptor;
 
     public OptionGroup toOptionGroup(
             CreateTicketOptionRequest createTicketOptionRequest, Event event) {
@@ -22,5 +32,18 @@ public class TicketOptionMapper {
                 .isEssential(true)
                 .options(new ArrayList<>())
                 .build();
+    }
+
+    @Transactional(readOnly = true)
+    public GetTicketItemOptionResponse toGetTicketItemOptionResponse(
+            Long eventId, Long ticketItemId) {
+
+        TicketItem ticketItem = ticketItemAdaptor.queryTicketItem(ticketItemId);
+        ticketItem.checkEventId(eventId);
+        List<Long> optionGroupIds = ticketItem.getOptionGroupIds();
+        List<OptionGroup> optionGroups = optionGroupAdaptor.findAllByIds(optionGroupIds);
+
+        return GetTicketItemOptionResponse.from(
+                optionGroups.stream().map(TicketOptionResponse::from).toList());
     }
 }
