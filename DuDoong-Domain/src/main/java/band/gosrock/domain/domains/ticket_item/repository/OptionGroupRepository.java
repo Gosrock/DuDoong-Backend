799b2dfee3625addc9445000b0b0fe39503de5ba
kim-wonjin <sobaek2000@naver.com>
2023-02-01 03:47:02 +0900

    feature(OptionGroup) : 이벤트에 속한 옵션그룹 조회

--- DuDoong-Domain/src/main/java/band/gosrock/domain/domains/ticket_item/repository/OptionGroupRepository.java
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/ticket_item/repository/OptionGroupRepository.java
@@ -3,6 +3,7 @@ package band.gosrock.domain.domains.ticket_item.repository;
 
 import band.gosrock.domain.domains.ticket_item.domain.OptionGroup;
 import band.gosrock.domain.domains.ticket_item.domain.OptionGroupStatus;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,6 @@ public interface OptionGroupRepository extends JpaRepository<OptionGroup, Long> 
 
     Optional<OptionGroup> findByIdAndOptionGroupStatus(
             Long optionGroupId, OptionGroupStatus status);
+
+    List<OptionGroup> findAllByEvent_IdAndOptionGroupStatus(Long eventId, OptionGroupStatus status);
 }
