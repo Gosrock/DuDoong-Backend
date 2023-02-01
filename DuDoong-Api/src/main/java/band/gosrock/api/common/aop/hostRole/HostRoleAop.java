43401684f72f4754d50df15c2ed61a6e34eac116
Chan Jin <water0641@naver.com>
2023-02-01 15:09:46 +0900

    refactor : host role Host 용 생성, 어노테이션 필드 리팩토링 (#256)
    * refactor : host role Host 용 생성
    
    * style : findhostFrom 카멜적용

--- DuDoong-Api/src/main/java/band/gosrock/api/common/aop/hostRole/HostRoleAop.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/common/aop/hostRole/HostRoleAop.java
@@ -1,4 +1,4 @@
-package band.gosrock.api.common.aop;
+package band.gosrock.api.common.aop.hostRole;
 
 
 import java.lang.reflect.Method;
@@ -20,34 +20,35 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @ConditionalOnExpression("${ableHostRoleAop:true}")
 public class HostRoleAop {
-    private final HostRoleTransaction hostRoleTransaction;
+    private final HostCallTransactionFactory hostCallTransactionFactory;
 
     /**
      * master 호스트의 마스터 manager 호스트의 수정,조회 ( 호스트유저도메인의 슈퍼 호스트 ) guest 호스트의 조회권한 (호스트유저도메인의 호스트 )
      *
      * @see band.gosrock.domain.domains.host.domain.HostRole
      */
-    @Around("@annotation(band.gosrock.api.common.aop.HostRolesAllowed)")
+    @Around("@annotation(band.gosrock.api.common.aop.hostRole.HostRolesAllowed)")
     public Object aop(final ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
         HostRolesAllowed annotation = method.getAnnotation(HostRolesAllowed.class);
-        String role = annotation.value();
-
+        HostQualification hostQualification = annotation.role();
         // 제공된 호스트의 role 이 정의된 세개의 롤과 같은지 확인한다.
         // 없으면 IllegalArgumentException 발생
-        HostQualification hostQualification = HostQualification.valueOf(role);
+        FindHostFrom findHostFrom = annotation.findHostFrom();
+        String identifier = findHostFrom.getIdentifier();
 
-        String identifier = annotation.eventIdIdentifier();
         String[] parameterNames = signature.getParameterNames();
         Object[] args = joinPoint.getArgs();
-        // 이벤트 아이디를 인자에서 얻어온다.
-        Long eventId = getEventId(parameterNames, args, identifier);
 
-        return hostRoleTransaction.proceed(eventId, hostQualification, joinPoint);
+        Long id = getId(parameterNames, args, identifier);
+
+        return hostCallTransactionFactory
+                .getCallTransaction(findHostFrom)
+                .proceed(id, hostQualification, joinPoint);
     }
 
-    public Long getEventId(String[] parameterNames, Object[] args, String paramName) {
+    public Long getId(String[] parameterNames, Object[] args, String paramName) {
         for (int i = 0; i < parameterNames.length; i++) {
             if (parameterNames[i].equals(paramName)) {
                 // 롱타입이라 가정. 안되면 classCastException 터트림
