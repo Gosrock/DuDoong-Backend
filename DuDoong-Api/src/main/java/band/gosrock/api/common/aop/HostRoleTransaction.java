633adf226228209bf565f26a74cc5323114057ff
Chan Jin <water0641@naver.com>
2023-02-01 12:31:25 +0900

    feat(host) : 호스트 권한 검증 aop (#250)
    * feat : HostRoles aop 추가
    
    * refactor : 호스트 검증 한곳 실제 적용
    
    * sytle: hostrolesAllowed 필드이름 role -> value 로 수정
    
    * fix: transactional 어노테이션 복구

---
+++ DuDoong-Api/src/main/java/band/gosrock/api/common/aop/HostRoleTransaction.java
@@ -0,0 +1,34 @@
+package band.gosrock.api.common.aop;
+
+
+import band.gosrock.api.common.UserUtils;
+import band.gosrock.domain.domains.event.adaptor.EventAdaptor;
+import band.gosrock.domain.domains.event.domain.Event;
+import band.gosrock.domain.domains.host.adaptor.HostAdaptor;
+import band.gosrock.domain.domains.host.domain.Host;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/** 호스트 정보를 트랜잭션 안에서 조회하기 위해서 만든 클래스입니다. 트랜잭션 내에서 캐시 할수 있으면 좋으니 이렇게 만들었습니다. - 이찬진 */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class HostRoleTransaction {
+
+    private final UserUtils userUtils;
+    private final EventAdaptor eventAdaptor;
+    private final HostAdaptor hostAdaptor;
+
+    @Transactional(readOnly = true)
+    public Object proceed(Long eventId, HostQualification role, final ProceedingJoinPoint joinPoint)
+            throws Throwable {
+        Long currentUserId = userUtils.getCurrentUserId();
+        Event event = eventAdaptor.findById(eventId);
+        Host host = hostAdaptor.findById(event.getHostId());
+        role.validQualification(currentUserId, host);
+        return joinPoint.proceed();
+    }
+}
