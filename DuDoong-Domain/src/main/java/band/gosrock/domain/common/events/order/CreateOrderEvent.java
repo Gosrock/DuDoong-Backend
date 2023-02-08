12ebcc392719d44063e496f5c7e559a211867671
Chan Jin <water0641@naver.com>
2023-02-08 19:46:33 +0900

    feature  : async error handler with slack alarm (#322)
    * feat : async 에러 처리 핸들러 추가
    
    * refactor : slack provider 분리
    
    * feat : slack sender 위치 재조정
    
    * feat : 비동기 에러 슬랙 핸들러 설정

--- DuDoong-Domain/src/main/java/band/gosrock/domain/common/events/order/CreateOrderEvent.java
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/common/events/order/CreateOrderEvent.java
@@ -6,10 +6,12 @@ import band.gosrock.domain.domains.order.domain.Order;
 import band.gosrock.domain.domains.order.domain.OrderMethod;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.lang.Nullable;
 
 @Getter
 @Builder
+@ToString
 public class CreateOrderEvent extends DomainEvent {
 
     private final String orderUuid;
