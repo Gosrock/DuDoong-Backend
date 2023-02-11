e7bd571d76a6014324c99bbdf0d0c9cf32a4b29a
gengminy <kls1998@naver.com>
2023-02-11 21:51:56 +0900

    feat : 호스트 슬랙 url 올바르지 않을 경우 예외처리

--- DuDoong-Infrastructure/src/main/java/band/gosrock/infrastructure/config/slack/SlackMessageProvider.java
+++ DuDoong-Infrastructure/src/main/java/band/gosrock/infrastructure/config/slack/SlackMessageProvider.java
@@ -4,8 +4,10 @@ package band.gosrock.infrastructure.config.slack;
 import com.slack.api.Slack;
 import com.slack.api.webhook.Payload;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -23,12 +25,30 @@ public class SlackMessageProvider {
 
     @Async
     public void sendMessage(String url, String text) {
-        if (url == null || url.isBlank()) return;
-        Slack slack = Slack.getInstance();
-        Payload payload = Payload.builder().text(text).username(username).iconUrl(iconUrl).build();
-
         try {
-            slack.send(url, payload);
+            doSend(url, text);
+        } catch (Exception ignored) {
+        }
+    }
+
+    /** 호스트가 존재하는 지 확인하기 위해 동기로 처리 */
+    public void register(String url) throws UnknownHostException {
+        final String text = "두둥 슬랙 알림이 성공적으로 등록되었습니다!";
+        doSend(url, text);
+    }
+
+    private void doSend(String url, String text) throws UnknownHostException {
+        final Slack slack = Slack.getInstance();
+        final Payload payload =
+                Payload.builder().text(text).username(username).iconUrl(iconUrl).build();
+        try {
+            String responseBody = slack.send(url, payload).getBody();
+            if (!StringUtils.equals(responseBody, "ok")) {
+                throw new UnknownHostException("올바른 슬랙 URL이 아닙니다.");
+            }
+        } catch (UnknownHostException error) {
+            // 호스트가 존재하지 않을 경우 abort
+            throw error;
         } catch (IOException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
