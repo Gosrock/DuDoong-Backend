a3c5f6183e259364c2b0c0e49e11b48229060b75
노경민 <kls1998@naver.com>
2023-02-22 13:51:13 +0900

    refactor : Enum 관련 코드 최적화 (#394)
    * refactor : EnumValidator 코드 최적화
    
    * feat : EnumClass 어노테이션 구현
    
    * refactor : 불필요 코드 제거

--- DuDoong-Domain/src/main/java/band/gosrock/domain/domains/event/domain/EventStatus.java
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/event/domain/EventStatus.java
@@ -1,40 +1,21 @@
 package band.gosrock.domain.domains.event.domain;
 
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import band.gosrock.common.annotation.EnumClass;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EnumClass
 public enum EventStatus {
-    // 준비중
     PREPARING("PREPARING", "준비중"),
-
-    // 진행중
     OPEN("OPEN", "진행중"),
-
-    // 정산중
     CALCULATING("CALCULATING", "정산중"),
-
-    // 지난 공연
     CLOSED("CLOSED", "지난공연"),
-
-    // 삭제된 공연
     DELETED("DELETED", "삭제된공연");
 
     private final String name;
     @JsonValue private final String value;
-
-    // Enum Validation 을 위한 코드, enum 에 속하지 않으면 null 리턴
-    @JsonCreator
-    public static EventStatus fromEventStatus(String val) {
-        for (EventStatus eventStatus : EventStatus.values()) {
-            if (eventStatus.name().equals(val)) {
-                return eventStatus;
-            }
-        }
-        return null;
-    }
 }
