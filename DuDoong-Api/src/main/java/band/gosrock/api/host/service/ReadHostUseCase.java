d5c2ce708f828477b9a27f25171206c00911efc3
gengminy <kls1998@naver.com>
2023-02-23 01:42:05 +0900

    refactor : 호스트 유저 정렬 순서 오래된 순서대로 유지

--- DuDoong-Api/src/main/java/band/gosrock/api/host/service/ReadHostUseCase.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/host/service/ReadHostUseCase.java
@@ -1,25 +1,19 @@
 package band.gosrock.api.host.service;
 
 
-import band.gosrock.api.common.UserUtils;
 import band.gosrock.api.host.model.dto.response.HostDetailResponse;
 import band.gosrock.api.host.model.mapper.HostMapper;
 import band.gosrock.common.annotation.UseCase;
-import band.gosrock.domain.domains.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
 @UseCase
 @RequiredArgsConstructor
 public class ReadHostUseCase {
-    private final UserUtils userUtils;
     private final HostMapper hostMapper;
 
     @Transactional(readOnly = true)
     public HostDetailResponse execute(Long hostId) {
-        final User user = userUtils.getCurrentUser();
-        final Long userId = user.getId();
-
         return hostMapper.toHostDetailResponse(hostId);
     }
 }
