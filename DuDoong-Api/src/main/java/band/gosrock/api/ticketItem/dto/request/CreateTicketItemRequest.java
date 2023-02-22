c53f97efcc778a900579690b57538f105290e1a2
Chan Jin <water0641@naver.com>
2023-02-23 01:13:59 +0900

    fix :  ticket create price zero 가능하게 설정 (#404)
    * fix : 가격 0원 요청 가능하도록 설정
    
    * style : spotless

--- DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/dto/request/CreateTicketItemRequest.java
+++ DuDoong-Api/src/main/java/band/gosrock/api/ticketItem/dto/request/CreateTicketItemRequest.java
@@ -7,7 +7,6 @@ import band.gosrock.domain.domains.ticket_item.domain.TicketType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.Nullable;
@@ -40,7 +39,7 @@ public class CreateTicketItemRequest {
     @Schema(nullable = true, example = "김원진")
     private String accountHolder;
 
-    @Positive
+    @NotNull
     @Schema(defaultValue = "0", nullable = false, example = "4000")
     private Long price;
 
