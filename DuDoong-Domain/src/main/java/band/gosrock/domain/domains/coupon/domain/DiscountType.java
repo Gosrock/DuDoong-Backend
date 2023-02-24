91c57de7b3c87d72a79f759e209bd3c1b72694d0
bryn <cofls6581@naver.com>
2023-02-24 19:00:09 +0900

    feat : @EnumClass 적용

--- DuDoong-Domain/src/main/java/band/gosrock/domain/domains/coupon/domain/DiscountType.java
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/coupon/domain/DiscountType.java
@@ -1,11 +1,13 @@
 package band.gosrock.domain.domains.coupon.domain;
 
 
+import band.gosrock.common.annotation.EnumClass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EnumClass
 public enum DiscountType {
     // 정액 할인
     AMOUNT("AMOUNT"),
