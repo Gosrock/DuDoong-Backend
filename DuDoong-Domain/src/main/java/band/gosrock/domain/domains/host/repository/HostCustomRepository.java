04b73677b96d64fc6aba208715acca4f78b3ad3c
gengminy <kls1998@naver.com>
2023-02-26 20:10:22 +0900

    fix : 내가 관리중인 이벤트 권한 적용

--- DuDoong-Domain/src/main/java/band/gosrock/domain/domains/host/repository/HostCustomRepository.java
+++ DuDoong-Domain/src/main/java/band/gosrock/domain/domains/host/repository/HostCustomRepository.java
@@ -2,9 +2,14 @@ package band.gosrock.domain.domains.host.repository;
 
 
 import band.gosrock.domain.domains.host.domain.Host;
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface HostCustomRepository {
     Slice<Host> querySliceHostsByUserId(Long id, Pageable pageable);
+
+    List<Host> queryHostsByActiveUserId(Long id);
+
+    Slice<Host> querySliceHostsByActiveUserId(Long id, Pageable pageable);
 }
