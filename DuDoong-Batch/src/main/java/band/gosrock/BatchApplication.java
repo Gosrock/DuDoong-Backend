e28a99f84a85d11b109ee3a6a29c50278553489a
이찬진 <water0641@naver.com>
2023-03-04 03:14:15 +0900

    fix : 배치어플레이션 웹서버 세팅 끄기

--- DuDoong-Batch/src/main/java/band/gosrock/BatchApplication.java
+++ DuDoong-Batch/src/main/java/band/gosrock/BatchApplication.java
@@ -3,13 +3,16 @@ package band.gosrock;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @EnableBatchProcessing
-@SpringBootApplication
 public class BatchApplication {
     public static void main(String[] args) {
-        final var context = SpringApplication.run(BatchApplication.class, args);
+        final var context =
+                new SpringApplicationBuilder(BatchApplication.class)
+                        .web(WebApplicationType.NONE)
+                        .run(args);
         System.exit(SpringApplication.exit(context));
     }
 }
